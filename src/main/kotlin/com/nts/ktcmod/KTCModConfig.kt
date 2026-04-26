841d94377d412c46c231fd8c69ef96aed1d60707
NoTuring_Steve <sysuntsofficial@outlook.com>
2026-04-26 14:40:10 +0800

    Update KTCModConfig.kt

--- src/main/kotlin/com/nts/ktcmod/KTCModConfig.kt
+++ src/main/kotlin/com/nts/ktcmod/KTCModConfig.kt
@@ -4,23 +4,23 @@ import net.neoforged.neoforge.common.ModConfigSpec
 
 object KTCModConfig {
     private val BUILDER = ModConfigSpec.Builder()
-    val ENABLE_PITY: ModConfigSpec.BooleanValue
-    val PITY_THRESHOLD: ModConfigSpec.IntValue
+    @JvmField val ENABLE_PITY: ModConfigSpec.BooleanValue
+    @JvmField val PITY_THRESHOLD: ModConfigSpec.IntValue
     @JvmField val DISABLE_WANDERING_TRADER: ModConfigSpec.BooleanValue
 
     init {
         BUILDER.push("disc_box")
         ENABLE_PITY = BUILDER
-            .comment("Enable pity system for disc box.")
+            .comment("config.${KTCMod.MODID}.pity1")
             .define("enablePity", true)
         PITY_THRESHOLD = BUILDER
-            .comment("Number of uses without disc21 before guarantee triggers")
+            .comment("config.${KTCMod.MODID}.pity2")
             .defineInRange("pityThreshold", 16, 1, 1000)
         BUILDER.pop()
 
         BUILDER.push("world_gen")
         DISABLE_WANDERING_TRADER = BUILDER
-            .comment("Disable Wandering Trader spawn. Only affects the trader itself, not naturally spawning llamas.")
+            .comment("config.${KTCMod.MODID}.disable_trader")
             .define("disableWanderingTrader", false)
         BUILDER.pop()
     }
